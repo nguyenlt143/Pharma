aa8f83449d71a5d84c8338d5fa11397be939118f
nguyenlt143 <nguyenlthe170273@fpt.edu.vn>
2025-10-23 21:49:42 +0700

    add more pharmacist controller code

--- src/main/java/vn/edu/fpt/pharma/controller/pharmacist/PharmacistController.java
+++ src/main/java/vn/edu/fpt/pharma/controller/pharmacist/PharmacistController.java
@@ -17,6 +17,37 @@ public class PharmacistController {
     @GetMapping("/pos/invoice")
     public String invoice(Model model) {
 
-        return "pages/pharmacist/list_invoice";
+        return "pages/pharmacist/invoice";
     }
+
+    @GetMapping("/pos/revenue")
+    public String revenue(Model model) {
+
+        return "pages/pharmacist/revenue";
+    }
+
+    @GetMapping("/pos/revenue/detail")
+    public String revenueDetail(Model model) {
+
+        return "pages/pharmacist/revenue_detail";
+    }
+
+    @GetMapping("/pos/shift")
+    public String shift(Model model) {
+
+        return "pages/pharmacist/shift";
+    }
+
+    @GetMapping("/pos/shiftDetail")
+    public String shiftDetail(Model model) {
+
+        return "pages/pharmacist/shift_detail";
+    }
+
+    @GetMapping("/pos/work")
+    public String workSchedule(Model model) {
+
+        return "pages/pharmacist/work_schedule";
+    }
+
 }
