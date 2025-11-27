8e3e044d0940e31253ca71c536d96b8bda1020c9
DuyNTHE171120 <duynthe171120@fpt.edu.vn>
2025-11-27 16:06:03 +0700

    Add conversion for variant

---
+++ src/main/java/vn/edu/fpt/pharma/repository/UnitConversionRepository.java
@@ -0,0 +1,14 @@
+package vn.edu.fpt.pharma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.edu.fpt.pharma.entity.UnitConversion;
+
+import java.util.List;
+
+@Repository
+public interface UnitConversionRepository extends JpaRepository<UnitConversion, Long> {
+    List<UnitConversion> findByVariantIdId(Long variantId);
+    void deleteByVariantIdId(Long variantId);
+}
+
